bbaf30da074a73e033fd0ec06a9a4233eddbe806
Ciaran <ciaran@usebounce.com>
2025-06-30 17:10:20 +0100

    chore: update expo to 53 (#445)

--- .storybook/main.js
+++ .storybook/main.js
@@ -2,14 +2,12 @@ module.exports = {
   stories: ["../src/**/*.stories.mdx", "../src/**/*.stories.@(js|jsx|ts|tsx)"],
   addons: [
     "@storybook/addon-links",
-    "@storybook/addon-essentials",
-    "@storybook/addon-interactions",
+    "@storybook/addon-webpack5-compiler-babel",
+    "@storybook/addon-docs",
   ],
   framework: {
     name: "@storybook/react-webpack5",
     options: {},
   },
-  docs: {
-    autodocs: true,
-  },
+  docs: {},
 }
